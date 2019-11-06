24ee77b10c291ea1b386263bec241a5f2d4532b1
Ulyana <ulyanamuravleva@gmail.com>
2019-11-06 13:32:14 +0300

    1.7.6 circle

--- app/src/main/java/com/example/ulyana1/MyView.java
+++ app/src/main/java/com/example/ulyana1/MyView.java
@@ -16,11 +16,13 @@ public class MyView extends View {
     protected void onDraw(Canvas canvas) { // создаем метод
         super.onDraw(canvas);
         Paint paint = new Paint();
-        //###ГОРИЗОНТАЛЬНЫЕ ПОЛОСКИ
-        int y = 0;
-        while (y < canvas.getHeight()) {
-            canvas.drawLine(0, y, this.getWidth(), y, paint);
-            y += 30;
-        }
+        //### КРУГ
+        paint.setColor(Color.YELLOW);
+        paint.setStyle(Paint.Style.FILL);
+        canvas.drawCircle(300, 300, 200, paint);
+        paint.setColor(Color.RED);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(20);
+        canvas.drawCircle(300, 300, 200, paint);
     }
 }
