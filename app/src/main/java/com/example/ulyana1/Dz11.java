7d6294a3f5950dcc5ee9935f201627efcb54e702
Ulyana <ulyanamuravleva@gmail.com>
2019-11-08 10:28:54 +0300

    1.11(4)

--- app/src/main/java/com/example/ulyana1/Dz11.java
+++ app/src/main/java/com/example/ulyana1/Dz11.java
@@ -14,8 +14,7 @@ import android.widget.Button;
 import android.widget.GridLayout;
 import java.util.Random;
 
-public class Dz11 extends Activity implements OnClickListener,
-        OnLongClickListener {
+abstract class Dz11 extends Activity implements OnClickListener, OnLongClickListener {
 
     private int WIDTH = 20;
     private int HEIGHT = 20;
@@ -51,26 +50,28 @@ public class Dz11 extends Activity implements OnClickListener,
         return false;
     }
 
-    @Override
-    public void onClick(View v) {
-        Button tappedCell = (Button) v;
-        int tappedX = getX(tappedCell);
-        int tappedY = getY(tappedCell);
-        int color = 0;
-
-        for (int j = 0; j < WIDTH; j++) {
-            color = ((ColorDrawable) cells[tappedY][j].getBackground()).getColor();
-            if (color == Color.WHITE) cells[tappedY][j].setBackgroundColor(Color.BLACK);
-            else cells[tappedY][j].setBackgroundColor(Color.WHITE);
-        }
-        for (int i = 0; i < WIDTH; i++) {
-            color = ((ColorDrawable) cells[i][tappedX].getBackground()).getColor();
-            if (color == Color.WHITE) cells[i][tappedX].setBackgroundColor(Color.BLACK);
-            else cells[i][tappedX].setBackgroundColor(Color.WHITE);
-        }
-
+    int getX(View v)
+    {
+        return Integer.parseInt(((String) v.getTag()).split(",")[1]);
+    }
+    int getY(View v)
+    {
+        return  Integer.parseInt(((String)v.getTag()).split(",")[0]);
+    }
+    void makeCells() {
+        cells = new Button[HEIGHT][WIDTH];
+        GridLayout cellsLayout = (GridLayout) findViewById(R.id.CellsLayout);
+        cellsLayout.removeAllViews();
+        cellsLayout.setColumnCount(WIDTH);
+        for (int i = 0; i < HEIGHT; i++)
+            for (int j = 0; j < WIDTH; j++) {
+                LayoutInflater inflater = (LayoutInflater) getApplicationContext()
+                        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                cells[i][j] = (Button) inflater.inflate(R.layout.cell, cellsLayout, false);
+                cells[i][j].setOnClickListener(this);
+                cells[i][j].setOnLongClickListener(this);
+                cells[i][j].setTag(i + "," + j);
+                cellsLayout.addView(cells[i][j]);
+            }
     }
 }
-
-
-
