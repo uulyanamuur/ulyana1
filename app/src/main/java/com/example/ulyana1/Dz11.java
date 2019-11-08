0c053bb5ae8460ae3e5cd54fefbe0bc468e2faa5
Ulyana <ulyanamuravleva@gmail.com>
2019-11-08 10:12:10 +0300

    1.11(2)

--- app/src/main/java/com/example/ulyana1/Dz11.java
+++ app/src/main/java/com/example/ulyana1/Dz11.java
@@ -14,62 +14,86 @@ import android.widget.Button;
 import android.widget.GridLayout;
 import java.util.Random;
 
+import ru.samsung.itschool.book.cells.R;
 import task.Stub;
 import task.Task;
 
-public class Dz11 extends Activity implements OnClickListener,
-        OnLongClickListener {
+public class Dz11 extends Activity implements View.OnClickListener,
+        View.OnLongClickListener {
 
-    private int WIDTH = 20;
-    private int HEIGHT = 20;
-    private Button[][] cells;
+    private int WIDTH = 9;
+    private int HEIGHT = 14;
+    private int[][] cells = new int[14][9];
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cells);
         makeCells();
+
         generate();
     }
-        int rand(){
-            Random random=new Random();
 
-            return (int)(Math.random()*10);
-        }
-        void generate() {
+    int rand() {
+        Random random = new Random();
 
-            for (int i = 0; i < HEIGHT; i++)
-                for (int j = 0; j < WIDTH; j++) {
-                    cells[i][j].setOnClickListener(this);
-                    cells[i][j].setOnLongClickListener(this);
+        return (int) (Math.random() * 10);
+    }
+
+    void generate()
+    {
+        int num = 1;
+
+        for(int j=0;j<WIDTH;j++)
+        {
+            for(int i=0;i< HEIGHT;i++)
+            {
+                //colCell[i][j] = (int)(Math.random()*3);
+                if(Math.random()>= 0.5)
+                {
+                    cells[i][j].setBackgroundColor(Color.BLUE);
                 }
-            for(int i=0;i<5;i++){
-                cells[rand()][rand()].setBackgroundColor(Color.BLACK);
+                cells[i][j].setText(num + " ");
+                num++;
             }
         }
-    @Override
-    public boolean onLongClick(View v) {
-        return false;
     }
     @Override
     public void onClick(View v) {
         Button tappedCell = (Button) v;
-        //Получаем координтаты нажатой клетки
+
         int tappedX = getX(tappedCell);
         int tappedY = getY(tappedCell);
-        int color = 0;
-
-        for (int j = 0; j < WIDTH; j++) {
-            color = ((ColorDrawable)cells[tappedY][j].getBackground()).getColor();
-            if (color == Color.WHITE) cells[tappedY][j].setBackgroundColor(Color.BLACK);
-            else cells[tappedY][j].setBackgroundColor(Color.WHITE);
-        }
-        for (int i = 0; i < WIDTH; i++) {
-            color = ((ColorDrawable) cells[i][tappedX].getBackground()).getColor();
-            if (color == Color.WHITE) cells[i][tappedX].setBackgroundColor(Color.BLACK);
-            else cells[i][tappedX].setBackgroundColor(Color.WHITE);
-        }
-
+        cells[tappedY][tappedX].setBackgroundColor((Color.CYAN));
     }
 
+    @Override
+    public boolean onLongClick(View v) {
+        return false;
+    }
 
-
+    int getX(View v)
+    {
+        return Integer.parseInt(((String) v.getTag()).split(",")[1]);
+    }
+    int getY(View v)
+    {
+        return  Integer.parseInt(((String)v.getTag()).split(",")[0]);
+    }
+    void makeCells() {
+        cells = new Button[HEIGHT][WIDTH];
+        GridLayout cellsLayout = (GridLayout) findViewById(R.id.CellsLayout);
+        cellsLayout.removeAllViews();
+        cellsLayout.setColumnCount(WIDTH);
+        for (int i = 0; i < HEIGHT; i++)
+            for (int j = 0; j < WIDTH; j++) {
+                LayoutInflater inflater = (LayoutInflater) getApplicationContext()
+                        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                cells[i][j] = (Button) inflater.inflate(R.layout.cell, cellsLayout, false);
+                cells[i][j].setOnClickListener(this);
+                cells[i][j].setOnLongClickListener(this);
+                cells[i][j].setTag(i + "," + j);
+                cellsLayout.addView(cells[i][j]);
+            }
+    }
+}
