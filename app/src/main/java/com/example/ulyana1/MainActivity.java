39b8e3745ec6fe98c61245ab460333f752f7de8d
Ulyana <ulyanamuravleva@gmail.com>
2019-11-10 21:19:44 +0300

    Saper

--- app/src/main/java/com/example/ulyana1/MainActivity.java
+++ app/src/main/java/com/example/ulyana1/MainActivity.java
@@ -11,7 +11,6 @@ import android.widget.Button;
 import android.widget.GridLayout;
 import android.graphics.Color;
 
-
 abstract class MainActivity extends Activity implements OnClickListener,
         OnLongClickListener {
 
